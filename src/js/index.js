1705bd9e1f85d7715bbc4164f4a38aada8d7a4b9
digital-souris <85878712+digital-souris@users.noreply.github.com>
2021-12-07 21:25:05 +0300

    123

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,20 @@
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
 
 let sTop = 0
+let scale = 1.2
 
 $(document).scroll(function () {
     let scrollPos = $(window).scrollTop()
+    let image = $('.first__image')
+    if (image.length) {
+        image = $(image).find('img')
+        let sectionH = $('.section_first').height()
+        if (scrollPos < sectionH) {
+            let percent = (sectionH - scrollPos) * 100 / sectionH / 5 / 100
+            console.log(percent)
+            image.css({'transform': `scale(${1.2 - (0.2 - percent)})`})
+        }
+    }
     if (scrollPos > 100) {
         $('.header').addClass('header_bg')
         if (sTop < scrollPos) {
